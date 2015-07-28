064b6511564336a1ed91c142197aff335fc035ae
Robert Widmann <devteam.codafi@gmail.com>
2015-07-28 09:46:11 -0600

    Redo LeftModule instances

--- Algebra/Module.swift
+++ Algebra/Module.swift
@@ -28,24 +28,24 @@ extension Int : LeftModule {
 	typealias A = Int
 }
 public func *<>(l : Int, r : Int) -> Int { return l * r }
-//extension Int8 : LeftModule {
-//	typealias R = Int
-//	typealias A = Int8
-//}
-//public func *<>(l : Int, r : Int8) -> Int8 { return Int8(l) * r }
-//extension Int16 : LeftModule {
-//	typealias R = Int
-//	typealias A = Int16
-//}
-//public func *<>(l : Int, r : Int16) -> Int16 { return Int8(l) * r }
-//extension Int32 : LeftModule {
-//	typealias R = Int
-//	typealias A = Int32
-//}
-//public func *<>(l : Int, r : Int32) -> Int32 { return Int8(l) * r }
-//extension Int64 : LeftModule {
-//	typealias R = Int
-//	typealias A = Int64
-//}
-//public func *<>(l : Int, r : Int64) -> Int64 { return Int8(l) * r }
-//
+extension Int8 : LeftModule {
+	typealias R = Int
+	typealias A = Int8
+}
+public func *<>(l : Int, r : Int8) -> Int8 { return Int8(l) * r }
+extension Int16 : LeftModule {
+	typealias R = Int
+	typealias A = Int16
+}
+public func *<>(l : Int, r : Int16) -> Int16 { return Int16(l) * r }
+extension Int32 : LeftModule {
+	typealias R = Int
+	typealias A = Int32
+}
+public func *<>(l : Int, r : Int32) -> Int32 { return Int32(l) * r }
+extension Int64 : LeftModule {
+	typealias R = Int
+	typealias A = Int64
+}
+public func *<>(l : Int, r : Int64) -> Int64 { return Int64(l) * r }
+
